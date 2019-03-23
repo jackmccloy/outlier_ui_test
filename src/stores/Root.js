5b8bf71609aa7e81e8bce5afc431c0bf338c331f
Jack McCloy <mccloy@me.com>
2019-03-23 18:12:12 -0400

    feat: Add package `redux-devtools-extension`

--- src/stores/Root.js
+++ src/stores/Root.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers } from 'redux';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import launchCollection from './LaunchCollectionReducer';
 
 const rootReducer = combineReducers({
   launchCollection
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
